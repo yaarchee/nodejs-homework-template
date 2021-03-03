833fcb9e6a91c0d814509d0b2a34b117907c0f30
Yaroslav Paltsun <trancemanjake@gmail.com>
2021-03-03 20:55:44 +0200

    some

---
+++ model/schemas/user.js
@@ -0,0 +1,63 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const { Sex } = require("../../utils/constants");
+
+const SALT_WORK_FACTOR = 8;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      minlength: 2,
+      default: "Guest",
+    },
+    sex: {
+      type: String,
+      enum: {
+        values: [Sex.MALE, Sex.FEMALE, Sex.NONE],
+        message: "It isn't allowed",
+      },
+      default: Sex.NONE,
+    },
+    email: {
+      type: String,
+      required: [true, "Email required"],
+      unique: true,
+      validate(value) {
+        const re = /\S+@\S+\.\S+/;
+        return re.test(String(value).toLowerCase());
+      },
+    },
+    password: {
+      type: String,
+      required: [true, "Password required"],
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+  this.password = await bcrypt.hash(this.password, salt, null);
+  next();
+});
+// userSchema.path('email').validate(function (value) {
+//   const re = /\S+@\S+\.\S+/
+//   return re.test(String(value).toLowerCase())
+// })
+
+userSchema.methods.validPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = model("user", userSchema);
+
+module.exports = User;
